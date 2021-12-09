c6f12e0ed1bc4bdec079115dd451de4cb2b252b0
bhavana94suresh <bhavana94suresh@gmail.com>
2021-12-09 06:25:50 +0400

    intial commit

---
+++ src/main/java/com/obsqura/basics/Sample.java
@@ -0,0 +1,29 @@
+package com.obsqura.basics;
+
+public class Sample {
+    public static void main(String args[]) {
+            System.out.println("hello");
+         findEven();
+
+    }
+       public static void findEven()
+        {
+
+            for(int i=1;i<16;i++)
+            {
+                if(i%2==0){
+                    System.out.println(i+"");
+
+
+                }
+
+            }
+
+        }
+
+    }
+
+
+
+
+
